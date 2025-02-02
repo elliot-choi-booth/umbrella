d04a67b1c2bc4ce359a67cb40ac016e839db8367
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-02 22:15:14 +0000

    adding in next twelve hours check for rain

--- umbrella.rb
+++ umbrella.rb
@@ -45,3 +45,34 @@ summary = currently.fetch("summary")
 temperature = currently.fetch("temperature")
 
 pp "It is currently #{temperature} degrees outside, and #{summary.downcase}."
+
+hourly = pm_parsed.fetch("hourly")
+hourly_data = hourly.fetch("data")
+
+next_twelve_hours = hourly_data[1..12]
+
+precip_prob_threshold = 0.10
+
+any_precipitation = false
+
+next_twelve_hours.each do |hour_hash|
+  precip_prob = hour_hash.fetch("precipProbability")
+
+  if precip_prob > precip_prob_threshold
+    any_precipitation = true
+
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+
+    puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
+  end
+end
+
+if any_precipitation == true
+  puts "You might want to take an umbrella!"
+else
+  puts "You probably won't need an umbrella."
+end
