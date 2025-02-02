354587fd26144f592c663a58b0df26ddaeea9fb4
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-02 04:54:34 +0000

    geocoding working

--- umbrella.rb
+++ umbrella.rb
@@ -9,13 +9,26 @@ pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_
 
 pp "Where are you located?"
 
-#user_location = gets.chomp
-user_location = "Chicago"
+user_location = gets.chomp.gsub(" ", "%20")
+#user_location = "Chicago"
 
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location +"&key=" + ENV.fetch("GMAPS_KEY") 
 
 response = HTTP.get(maps_url)
-pp response
 
+raw_response = response.to_s
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+geo = first_result.fetch("geometry")
+
+location = geo.fetch("location")
+
+pp latitude = location.fetch("lat")
+pp longitude = location.fetch("lng")
