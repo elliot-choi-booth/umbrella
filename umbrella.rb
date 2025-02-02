6be66f217a8df1ee4c61a44acaa3c2b833289cbe
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-02 04:38:31 +0000

    adding in pirate weather api key fetch

--- umbrella.rb
+++ umbrella.rb
@@ -3,4 +3,8 @@ require "http"
 require "json"
 require "dotenv/load"
 
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+
+pp "howdy"
