5af31dbc97ee93f104f435c929ee8e7e283b173d
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-02 04:46:07 +0000

    fixing api keys and adding gmaps

--- umbrella.rb
+++ umbrella.rb
@@ -7,4 +7,15 @@ pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
 
-pp "howdy"
+pp "Where are you located?"
+
+#user_location = gets.chomp
+user_location = "Chicago"
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location +"&key=" + ENV.fetch("GMAPS_KEY") 
+
+response = HTTP.get(maps_url)
+pp response
+
